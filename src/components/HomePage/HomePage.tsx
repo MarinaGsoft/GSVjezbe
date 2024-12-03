76af44f0588a82019f46f62bbc1ec4eb72e0c610
FranoJurkovic <jurkovicfrano2000@gmail.com>
2024-12-03 18:31:43 +0100

    1.0

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,24 +1,88 @@
-import React, { FC } from "react";
-import logo from "../../logo.svg";
+import React, { FC, useState } from "react";
 import "./HomePage.scss";
 
-export const HomePage: FC = () => {
+type HomePageProps = {
+  age?: number;
+};
+
+export const HomePage: FC<HomePageProps> = ({ age = 24 }) => {
+  const [disable, setDisable] = useState(false);
+  const [count, setCount] = useState(0);
+  const [disabled, setDisabled] = useState(false);
+  const [theme, setTheme] = useState("day");
+
+  const currentDate = new Date().toLocaleDateString();
+  const currentTime = new Date().toLocaleTimeString();
+
+  const onClick = () => {
+    console.log("vježba 2");
+    setDisable(true);
+  };
+
+  const countup = () => {
+    console.log("vježba 5");
+    setCount(count + 1);
+    if (count >= 10) {
+      setDisabled(true);
+    }
+  };
+
+  const countdown = () => {
+    console.log("vježba 5");
+    setCount(count - 1);
+    if (count <= 0) {
+      setDisabled(true);
+    }
+  };
+
+  const toggleTheme = () => {
+    setTheme(theme === "day" ? "night" : "day");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`homepage theme--${theme}`}>
+      <div className="homepage-content">
+        <p>1. vježba</p>
+        <h1>Dobrodošli na radionicu.</h1>
+      </div>
+
+      <div className="homepage-content">
+        <p>2. vježba</p>
+        <button className="gumb" onClick={onClick} disabled={disable}>
+          Dodaj gumb
+        </button>
+      </div>
+
+      <div className="homepage-content">
+        <p>3. vježba</p>
+        <h2>
+          Danas je {currentDate} i trenutno je {currentTime}
+        </h2>
+      </div>
+
+      <div className="homepage-content">
+        <p>4. vježba</p>
+        <h2>{age >= 18 ? "Punoljetni ste" : "Niste punoljetni"}</h2>
+      </div>
+
+      <div className="homepage-content">
+        <p>5. vježba</p>
+        <button onClick={countup} disabled={disabled}>
+          +
+        </button>
+        <button onClick={countdown} disabled={disabled}>
+          -
+        </button>
+        <p>{count}</p>
+      </div>
+
+      <div className="homepage-content">
+        <p>6. vježba</p>
+        <button onClick={toggleTheme} className="theme">
+          Promjeni u {theme === "day" ? "night" : "day"}
+        </button>
+        <p>{theme}</p>
+      </div>
     </div>
   );
 };
